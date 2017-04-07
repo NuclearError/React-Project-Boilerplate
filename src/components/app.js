554fb4cdc5123bc1c74dd5fb06e0b38933033eff
Angela Cullen <angela.cullen@immediate.co.uk>
2017-04-07 18:03:06 +0100

    Revert "Atom.io had some unsaved files, am now committing"
    This reverts commit ae59da980fd10509e92cf60c0ee0c1e007c1225f.

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
 
-import BookList from '../containers/book-list';
-import BookDetail from '../containers/book-detail';
+import Calendar from './calendar';
+//import BookDetail from '../containers/book-detail';
 
 export default class App extends Component {
+/*
+  constructor(props) {
+		super(props);
+
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
+	};
+*/
+
   render() {
     return (
+      <div className='hello'>
+        <h1>Kynareth</h1>
+        <Calendar />
+      </div>
+      /*
       <section className='book-list__container'>
         <BookList />
         <BookDetail />
       </section>
+      */
     );
   }
+
 }
