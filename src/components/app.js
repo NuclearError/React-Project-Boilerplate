74418769b641c39f5f5a022e34cb01d78c6045d7
Angela Cullen <angela.cullen@immediate.co.uk>
2017-04-07 18:12:00 +0100

    Deleted old placeholder files, set up new logic to pull months list into calendar component

--- src/components/app.js
+++ src/components/app.js
@@ -1,19 +1,8 @@
 import React, { Component } from 'react';
 
 import Calendar from './calendar';
-//import BookDetail from '../containers/book-detail';
 
 export default class App extends Component {
-/*
-  constructor(props) {
-		super(props);
-
-		this.state = {
-			videos: [],
-			selectedVideo: null
-		};
-	};
-*/
 
   render() {
     return (
@@ -21,12 +10,6 @@ export default class App extends Component {
         <h1>Kynareth</h1>
         <Calendar />
       </div>
-      /*
-      <section className='book-list__container'>
-        <BookList />
-        <BookDetail />
-      </section>
-      */
     );
   }
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
-import BooksReducer from './reducer_books';
-import ActiveBook from './reducer_active_book';
+import Months from './months';
 
 const rootReducer = combineReducers({
-  books: BooksReducer,
-  activeBook: ActiveBook
+  months: Months
 });
 
 export default rootReducer;
