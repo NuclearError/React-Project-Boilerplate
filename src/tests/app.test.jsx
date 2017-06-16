3ce64d97ff6db1fb25e32474adafb879ee0c1d18
Angela Cullen <angela.cullen@immediate.co.uk>
2017-06-16 14:53:39 +0100

    Added basic jest/enzyme unit tests

---
+++ src/tests/app.test.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../components/app';
+
+describe('<App /> component', () => {
+
+  // variable declarations here
+  let app;
+
+  beforeEach( () => {
+    app = shallow(<App />);
+  });
+
+  it('renders on the page', () => {
+    expect(app.length).toBe(1);
+    // expect(app).toMatchSnapshot();
+  });
+
+});
