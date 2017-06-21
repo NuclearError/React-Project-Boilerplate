7c194aaa9e9ff50423c40801cf1c14a752c8ed7c
Angela Cullen <esmearmitage@gmail.com>
2017-06-21 12:40:19 +0100

    Cleaned up indentation

--- src/components/task.js
+++ src/components/task.js
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types';
 import Item from './item';
 
 const Task = props => (
-    <li className="task__item">
-        {props.variety !== '' ?
-            <span className="task__item-variety">{props.variety} </span>
-            :
-            ''
-        }
-        <span className="task__item-name">{props.name}</span>
-    </li>
+  <li className="task__item">
+    {props.variety !== '' ?
+      <span className="task__item-variety">{props.variety} </span>
+      :
+      ''
+    }
+    <span className="task__item-name">{props.name}</span>
+  </li>
 );
 
 Task.defaultProps = {
--- src/containers/task-group.js
+++ src/containers/task-group.js
@@ -5,70 +5,70 @@ import Task from '../components/task';
 
 class TaskGroup extends Component {
 
-    processTaskList(list, givenMonth, givenType) {
-          let resultsArray = [];
-          const loop = list.map( (item) => {
-              if(item.month === givenMonth && item.type === givenType) {
-                  resultsArray.push(item);
-              };
-          });
-          if(resultsArray.length) {
-              return true;
-          }
+  processTaskList(list, givenMonth, givenType) {
+    let resultsArray = [];
+    const loop = list.map( (item) => {
+      if(item.month === givenMonth && item.type === givenType) {
+        resultsArray.push(item);
+      };
+    });
+    if(resultsArray.length) {
+      return true;
     }
+  }
 
   taskTypeIsUsed(list, givenMonth, givenType) {
-      let taskIsUsed = false;
+    let taskIsUsed = false;
 
-     // verification is an array containing lots of
-     // 'undefined' + occasionally true boolean
+    // verification is an array containing lots of
+    // 'undefined' + occasionally true boolean
     const verification = list.map( (item) => {
-        if(this.processTaskList(item.tasks, givenMonth, givenType) !== undefined) {
-            taskIsUsed = true;
-        }
+      if(this.processTaskList(item.tasks, givenMonth, givenType) !== undefined) {
+        taskIsUsed = true;
+      }
     });
 
     return taskIsUsed;
   };
 
   taskHasItems(list, givenMonth, givenType) {
-      return this.processTaskList(list, givenMonth, givenType);
+    return this.processTaskList(list, givenMonth, givenType);
   };
 
   renderTask(taskType) {
-      const taskClass = taskType.toLowerCase().replace(" ", "-");
+    const taskClass = taskType.toLowerCase().replace(" ", "-");
 
-      if(this.taskTypeIsUsed(this.props.data, this.props.thisMonth, taskType)) {
-          return (
-            <li className={`task task--${taskClass}`}>
-                <h3 className={`task__title task__title--${taskClass}`}>{taskType}</h3>
-                <ul className="task__item-list">
-                    {this.renderTaskItems(taskType)}
-                </ul>
-            </li>
-          );
-      }
+    if(this.taskTypeIsUsed(this.props.data, this.props.thisMonth, taskType)) {
+      return (
+        <li className={`task task--${taskClass}`}>
+          <h3 className={`task__title task__title--${taskClass}`}>{taskType}</h3>
+          <ul className="task__item-list">
+            {this.renderTaskItems(taskType)}
+          </ul>
+        </li>
+      );
+    }
   };
 
   renderTaskItems(taskType) {
-      return this.props.data.map( (item) => {
-          if(this.taskHasItems(item.tasks, this.props.thisMonth, taskType)) {
-              return (
-                  <Task variety={item.variety} name={item.name} />
-              );
-          }
-      });
+    return this.props.data.map( (item) => {
+      if(this.taskHasItems(item.tasks, this.props.thisMonth, taskType)) {
+        return (
+          <Task variety={item.variety} name={item.name} />
+        );
+      }
+    });
   };
 
   //  Refactor tasks below to loop through a reducer list
   render() {
     return (
-        <ul className="task-container">
-            {this.renderTask("Sow Indoors")}
-            {this.renderTask("Sow Outside")}
-            {this.renderTask("Plant Outside")}
-            {this.renderTask("Harvest")}
-        </ul>
+      <ul className="task-container">
+        {this.renderTask("Sow Indoors")}
+        {this.renderTask("Sow Outside")}
+        {this.renderTask("Plant Outside")}
+        {this.renderTask("Harvest")}
+      </ul>
     );
   };
 
